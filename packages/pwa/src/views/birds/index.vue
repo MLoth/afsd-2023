7cc19ba842abc924b92208ff4c861fccbe20c0ac
MLoth <MLoth@users.noreply.github.com>
2023-10-03 14:40:58 +0200

    feat: added bird details & pictures

--- packages/pwa/src/views/birds/index.vue
+++ packages/pwa/src/views/birds/index.vue
@@ -2,18 +2,35 @@
   <Container>
     <h1 class="text-4xl font-bold tracking-wide mb-6">Birds</h1>
 
-    <p>Birds</p>
-
     <div v-if="birdsLoading">Loading...</div>
     <div v-if="birdsError">
       {{ birdsError }}
     </div>
-    <div v-if="birds">
-      <ul>
-        <li v-for="bird in birds.birds">
-          {{ bird.name }}
-        </li>
-      </ul>
+
+    <div
+      v-if="birds"
+      class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 grid-rows-auto gap-12"
+    >
+      <div v-for="bird in birds.birds" class="flex items-center gap-6">
+        <img
+          class="w-48"
+          :src="`/birds/${bird.name}.webp`"
+          :alt="`Picture of ${bird.name}.`"
+        />
+        <div>
+          <h2 class="text-lg font-semibold tracking-wide">
+            {{ bird.name }}
+          </h2>
+          <p class="text-xs tracking-wide mb-2">{{ bird.category }}</p>
+          <p class="text-sm">{{ bird.observations }} times spotted</p>
+
+          <RouterLink
+            class="flex items-center text-sm text-blue-600 hover:underline mt-2"
+            :to="`/birds/${bird.name}`"
+            >{{ bird.name }} <ArrowRightIcon class="h-4"
+          /></RouterLink>
+        </div>
+      </div>
     </div>
   </Container>
 </template>

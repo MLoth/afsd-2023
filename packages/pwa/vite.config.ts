294a59f3dc5ea9c21ca7d039c5da7e1a59b459e2
Marty <martijn.loth@icloud.com>
2023-10-11 14:25:43 +0200

    feat: added PWA support

--- packages/pwa/vite.config.ts
+++ packages/pwa/vite.config.ts
@@ -3,10 +3,38 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [
+    vue(),
+    UnoCSS(),
+    VitePWA({
+      manifest: {
+        name: 'Birds observations',
+        short_name: 'Birds',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+      },
+    }),
+  ],
 
   resolve: {
     alias: {

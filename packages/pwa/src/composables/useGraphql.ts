dc948dcc3e906d5aa0f1eac4ceb2cfbd37134396
MLoth <MLoth@users.noreply.github.com>
2023-11-08 15:23:07 +0100

    feat: welcomed newest user!

--- packages/pwa/src/composables/useGraphql.ts
+++ packages/pwa/src/composables/useGraphql.ts
@@ -3,10 +3,17 @@ import {
   createHttpLink,
   from,
   InMemoryCache,
+  HttpLink,
+  split,
 } from '@apollo/client/core'
+import { getMainDefinition } from '@apollo/client/utilities'
+
 import { setContext } from '@apollo/client/link/context'
 import { onError } from '@apollo/client/link/error'
 import { logErrorMessages } from '@vue/apollo-util'
+
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
 
 import useFirebase from './useFirebase'
 
@@ -31,13 +38,32 @@ const authLink = setContext(async (_, { headers }) => ({
   },
 }))
 
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:3001/graphql',
+  }),
+)
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  from([authLink, errorLink, httpLink]),
+)
+
 const apolloClient = new ApolloClient({
-  link: from([authLink, errorLink, httpLink]),
+  link,
   cache: new InMemoryCache(), // <- this is where the cache is created
 })
-
 export default () => {
   return {
     apolloClient,
+    wsLink,
   }
 }
